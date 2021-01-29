21466e564128e0d27c41228cff001a3e015fdd65
Bradley Williams <skyski88@gmail.com>
2021-01-29 15:11:05 -0500

    commit

---
+++ main/build/precache-manifest.31c88c31afe54e03bb759849a6f01536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73b051f9a539bb4b2e36",
+    "url": "/static/js/main.73b051f9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "73b051f9a539bb4b2e36",
+    "url": "/static/css/main.7d4c34e9.chunk.css"
+  },
+  {
+    "revision": "0a24afa7123456bc497530cc5f46fb45",
+    "url": "/index.html"
+  }
+];
